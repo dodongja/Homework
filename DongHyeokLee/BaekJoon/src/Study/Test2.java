2d08bc4601a9fbfb86426e588f952d54e4618fbe
dodongja <qs553@nate.com>
2022-05-18 11:47:26 +0900

    python 복습

---
+++ DongHyeokLee/BaekJoon/src/Study/Test2.java
@@ -0,0 +1,2 @@
+package Study;public class Test2 {
+}
---
+++ DongHyeokLee/Spring/demo/src/main/java/com/example/demo/python/PythonRequester.java
@@ -0,0 +1,11 @@
+package com.example.demo.python;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/python-request")
+public class PythonRequseter {
+}
